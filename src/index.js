fd2ec3dc207a6950b453fb7e17c89a2ae82907c4
Alex Aragon <alexaragon@MacBook-Pro-2.local>
2017-03-28 21:28:41 -0700

    WIP

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './assets/img/hero-driftwood-sm.jpg';
+import './assets/img/hero-driftwood.jpg';
+import './assets/img/KClogo-horiz-black-lg.gif';
+import './assets/img/KClogo-horiz-white-sm-xs.png';
+import "./assets/css/kc-theme-corporate.css";
+import "./assets/css/custom.css";
+import "./js/main.js"
+import "./js/onoffcanvas.js";
